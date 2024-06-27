60511cd54c4534d553dd7506c7a6bc213087707d
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-06-27 11:05:08 +0400

    Add files via upload

---
+++ apple.py
@@ -0,0 +1,16 @@
+"""
+    Plz like I guess? 🚱
+"""
+
+Int1 = int(input(""))
+try:
+    print(Int1)
+except ValueError:
+    print('Enter a Int')
+
+print("If you where to cut an apple pie in", Int1, "slices...")
+print("You would have")
+print(100 / Int1, "% per slice")
+print("👍")
+
+input("Press ENTER to exit")
